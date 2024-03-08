22096f1e43237b37760611ba72aafc385b11de53
laryrwin <larry.nguyen726@gmail.com>
2024-03-07 21:07:09 -0800

    created env

--- db-connector.js
+++ db-connector.js
@@ -1,14 +1,16 @@
-// Get an instance of mysql we can use in the app
-var mysql = require('mysql')
+// Load environment variables from .env file
+require('dotenv').config();
+
+var mysql = require('mysql');
 
 // Create a 'connection pool' using the provided credentials
 var pool = mysql.createPool({
     connectionLimit : 10,
-    host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_nguyelar',
-    password        : '9751',
-    database        : 'cs340_nguyelar'
-})
+    host            : process.env.DB_HOST,
+    user            : process.env.DB_USER,
+    password        : process.env.DB_PASSWORD,
+    database        : process.env.DB_DATABASE
+});
 
 // Export it for use in our application
 module.exports.pool = pool;
